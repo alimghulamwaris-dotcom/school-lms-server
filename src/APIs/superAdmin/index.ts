af7e1bb8dd10c22bee0c7e9a6bbf37d0bc9a15e8
Aliwarisss <alimaw785@gmail.com>
2026-04-09 12:27:51 +0500

    fixes 5

--- src/APIs/superAdmin/index.ts
+++ src/APIs/superAdmin/index.ts
@@ -6,7 +6,7 @@ import superAdminController from './superAdmin.controller'
 
 const router = Router()
 
-router.use(rateLimiter, authenticate, authorizeSuperAdmin)
+router.use('/super-admin', rateLimiter, authenticate, authorizeSuperAdmin)
 
 router.route('/super-admin/overview').get(superAdminController.overview)
 router.route('/super-admin/schools').get(superAdminController.listSchools).post(superAdminController.createSchool)
